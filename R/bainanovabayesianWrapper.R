ec274be190eafcfe1ccefa97718edf7fa684d92e
boutinb <b.boutin@uva.nl>
2024-05-28 15:35:11 +0200

    Update wrappers

--- R/bainanovabayesianWrapper.R
+++ R/bainanovabayesianWrapper.R
@@ -19,7 +19,7 @@
 
 BainAnovaBayesian <- function(
           data = NULL,
-          version = "0.18.2",
+          version = "0.19",
           bayesFactorMatrix = FALSE,
           bayesFactorPlot = FALSE,
           credibleInterval = 0.95,
