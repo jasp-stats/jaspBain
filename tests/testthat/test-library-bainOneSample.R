3e3d8a80844795d139bbee7513bb6602df16ed96
Koen Derks <koen-derks@hotmail.com>
2026-03-02 17:45:00 +0100

    Restructure unit tests

--- tests/testthat/test-library-bainOneSample.R
+++ tests/testthat/test-library-bainOneSample.R
@@ -1,12 +1,12 @@
-context("Example: bainOneSample")
+context("Library: bainOneSample")
 
 # This test file was auto-generated from a JASP example file.
-# The JASP file is stored in the module's examples/ folder.
+# The JASP file is stored in tests/testthat/jaspfiles/library/.
 
 test_that("BainTTestBayesianOneSample results match", {
 
   # Load from JASP example file
-  jaspFile <- testthat::test_path("..", "..", "examples", "bainOneSample.jasp")
+  jaspFile <- testthat::test_path("jaspfiles", "library", "bainOneSample.jasp")
   opts <- jaspTools::analysisOptions(jaspFile)
   dataset <- jaspTools::extractDatasetFromJASPFile(jaspFile)
 
